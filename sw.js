1b4b1a27bef442d69e9af6cfc50c13250dcc1e45
menelousoklid <menelousoklid@gmail.com>
2026-02-10 01:20:10 +0300

    Upgrade service worker cache version and enhance activation
    Updated cache version and added activate event listener for cache cleanup.

--- sw.js
+++ sw.js
@@ -1,12 +1,24 @@
-const CACHE = "saha-v1";
+const CACHE = "saha-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request).catch(()=>caches.match("./index.html")))
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request).catch(() => caches.match("./index.html"));
+    })
   );
 });
