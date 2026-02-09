5091f059e85f046df1608f1fec1e97f6b865ce47
menelousoklid <menelousoklid@gmail.com>
2026-02-10 01:31:31 +0300

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,24 +1,21 @@
-const CACHE = "saha-v2";
+const CACHE = "saha-cache-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(ASSETS))
-  );
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
-    )
-  );
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)));
+    await self.clients.claim();
+  })());
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request).catch(() => caches.match("./index.html"));
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request).catch(() => caches.match("./index.html")))
   );
 });
